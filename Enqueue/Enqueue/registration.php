c6e01c45d573a1b135ad94829ed008f1621ef0d5
Ruslan <Ruslan@MacBook-Pro.local>
2018-03-03 18:48:04 +0200

    Remove trashed folder.

--- Enqueue/Enqueue/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @author    Ruslan <rusalndovg291@gmail.com>
- */
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Enqueue_Enqueue',
-    __DIR__
-);
