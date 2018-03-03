c3a253bde7cc9de3150668444c6696503b771726
Ruslan <Ruslan@MacBook-Pro.local>
2018-03-03 18:20:34 +0200

    Update composer json
     - Update repo file structure

---
+++ Model/Config/Source/Transport.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Enqueue\Enqueue\Model\Config\Source;
+
+class Transport
+{
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return [
+            ['value' => 'rabbitmq_amqp', 'label' => __('RabbitMQ AMQP')],
+            ['value' => 'amqp', 'label' => __('AMQP')],
+            ['value' => 'rabbitmq_stomp', 'label' => __('RabbitMQ STOMP')],
+            ['value' => 'stomp', 'label' => __('STOMP')],
+            ['value' => 'fs', 'label' => __('Filesystem')],
+            ['value' => 'sqs', 'label' => __('Amazon AWS SQS')],
+            ['value' => 'redis', 'label' => __('Redis')],
+            ['value' => 'dbal', 'label' => __('Doctrine DBAL')],
+        ];
+    }
+}
---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author    Ruslan <rusalndovg291@gmail.com>
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Enqueue_Enqueue',
+    __DIR__
+);
